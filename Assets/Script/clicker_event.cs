b7768a5b82ccbe7baf303a83bf496439896f5f16
SeongJun-Ham <tjdwns8793@mensakorea.org>
2021-04-11 15:09:17 +0900

    Revert "hitpower, upgrade가격, testResetBtn(비트코인, hitpower 초기화 버튼)"
    This reverts commit dc8d3b125a28057cfba9a525b2f4674094ee07a9.

--- Assets/Script/clicker_event.cs
+++ Assets/Script/clicker_event.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class clicker_event : MonoBehaviour
 {
     private system scriptSystem;
-    private msgbox scriptMsgbox;
 
     // Start is called before the first frame update
     void Start()
@@ -22,27 +21,11 @@ public class clicker_event : MonoBehaviour
 
     public void clicker()
     {
-        scriptSystem.curBitcoin += 1.002f * 0.0000008f * (scriptSystem.hitPower - 1) + 0.0000008f;
+        scriptSystem.curBitcoin += scriptSystem.hitPower;
     }
 
     public void clicker_upgrade()
     {
-        if (!scriptMsgbox) scriptMsgbox = GameObject.Find("EventSystem").GetComponent<msgbox>();
-
-        if (scriptSystem.curBitcoin >= 0.000008f * scriptSystem.hitPower)
-        {
-            scriptSystem.curBitcoin -= 0.000008f * scriptSystem.hitPower;
-            scriptSystem.hitPower += 1;
-        }
-        else
-        {
-            scriptMsgbox.showMsgbox("You Can't Do That.", "¿¹");
-        }
-    }
-
-    public void hitPowerReset()
-    {
-        scriptSystem.curBitcoin = 0;
-        scriptSystem.hitPower = 1;
+        scriptSystem.hitPower = 1.002f * scriptSystem.hitPower + 0.0000008f;
     }
 }
