7313ed8dd056dde616fd116e116bf72a094a095f
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-04-11 19:09:14 +0900

    환경설정 버튼 구현 - SettingBtn

--- Assets/Script/settingBtn.cs
+++ Assets/Script/settingBtn.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
 
 public class settingBtn : MonoBehaviour
 {
+    public int clickedBtn;
+    private GameObject settingPanel, settingBtn1, settingBtn2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        settingPanel = GameObject.Find("Canvas").transform.Find("setting_panel").gameObject;
     }
 
     // Update is called once per frame
@@ -16,8 +21,46 @@ public class settingBtn : MonoBehaviour
         
     }
 
-    public void OnClickEvent()
+    async public void OnClickEvent()
     {
-        Debug.Log("Setting Click");
+        showSetting();
+        var task = Task.Run(() => getClickedBtn());
+        int clickedBtn = await task;
+
+        if(clickedBtn == 1)
+        {
+            //TODO: 세팅 저장
+        }
+    }
+
+    public void OnClickEventBtn1()
+    {
+        hideMsgboxYesOrNo();
+        clickedBtn = 1;
+    }
+    public void OnClickEventBtn2()
+    {
+        hideMsgboxYesOrNo();
+        clickedBtn = 2;
+    }
+
+    public void showSetting()
+    {
+        clickedBtn = -1;
+        settingPanel.SetActive(true);
+    }
+
+    private void hideMsgboxYesOrNo()
+    {
+        settingPanel.SetActive(false);
+    }
+
+    public int getClickedBtn()
+    {
+        while (clickedBtn == -1)
+        {
+
+        }
+        return clickedBtn;
     }
 }
