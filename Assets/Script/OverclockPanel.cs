341f00c50daa3d4150b1dd48e22144ff192fde78
SeongJun-Ham <tjdwns8793@mensakorea.org>
2021-04-24 00:06:28 +0900

    Clicker 이펙트 동전 떨어지게 만들기

--- Assets/Script/OverclockPanel.cs
+++ Assets/Script/OverclockPanel.cs
@@ -3,17 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class OverclockPanel : MonoBehaviour
 {
     private GameSystem scriptGameSystem;
     private Msgbox scriptMsgbox;
     private Tabpanel scriptTabpanel;
+    private Vector3 BTCPosition;
+    private float X_VELOCITY;
+    private float Y_VELOCITY;
+    public GameObject itemBTC;
+    public Vector3 mousePosition;
+    public Rigidbody2D rigid;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,7 +32,17 @@ public class OverclockPanel : MonoBehaviour
     public void OnClickEvent()
     {
         if (!scriptGameSystem) scriptGameSystem = GameObject.Find("GameSystem").GetComponent<GameSystem>();
-        scriptGameSystem.currentBtc += Convert.ToInt64(scriptGameSystem.BTC_AT_FIRST_TOUCH * (Math.Pow(scriptGameSystem.COEFFICIENT_OF_OVERCLOCK, scriptGameSystem.BIFURCATION_OF_OVERCLOCK) - 1) * scriptGameSystem.currentOverclockLevel / 75) + (20 * scriptGameSystem.BTC_AT_FIRST_TOUCH*10 / 21);
+        scriptGameSystem.currentBtc += Convert.ToInt64(scriptGameSystem.BTC_AT_FIRST_TOUCH * (Math.Pow(scriptGameSystem.COEFFICIENT_OF_OVERCLOCK, scriptGameSystem.BIFURCATION_OF_OVERCLOCK) - 1) * scriptGameSystem.currentOverclockLevel / 75) + (20 * scriptGameSystem.BTC_AT_FIRST_TOUCH * 10 / 21);
+        mousePosition = Input.mousePosition;
+        GameObject instantObject = Instantiate(itemBTC, this.mousePosition, transform.rotation) as GameObject;
+        instantObject.transform.parent = GameObject.Find("ClickerButton").transform;
+
+        Rigidbody2D itemRigid = instantObject.GetComponent<Rigidbody2D>();
+        X_VELOCITY = Random.Range(-5.0f, 5.0f);
+        Y_VELOCITY = Random.Range(30.0f, 50.0f);
+        Vector2 velocityVector = new Vector2(X_VELOCITY, Y_VELOCITY);
+        itemRigid.AddForce(velocityVector, ForceMode2D.Impulse);
+        Destroy(instantObject.gameObject, 1f);
     }
 
     public void UpgradeOverclock()
