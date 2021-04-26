f83df790ce02413c0f419be62f903448e4895a7d
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-04-26 23:11:28 +0900

    1. Ads 추가 2. Ads 추가에 따른 전반적인 사이즈 재조정 3. Scene - Pc 프리팹 오브젝트 삭제

---
+++ Assets/Script/AdsManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour
+{
+    private const string ANDROID_GAME_ID = "4105315";
+    private const string IOS_GAME_ID = "4105314";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+#if UNITY_ANDROID
+        Advertisement.Initialize(ANDROID_GAME_ID, true);
+#elif UNITY_IOS
+        Advertisement.Initialize(IOS_GAME_ID, true);
+#endif        
+    }
+
+    public void ShowAds()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show("video");
+        }
+        else
+        {
+            Debug.Log("Advertisement.IsReady() returned False");
+        }
+    }
+}
